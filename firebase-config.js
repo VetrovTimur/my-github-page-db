87da81834625b31e6d7742fa9b336a26f449f8d0
TimurVetrov <140169369+VetrovTimur@users.noreply.github.com>
2026-09-20 12:12:06 +0300

    Create firebase-config.js

---
+++ firebase-config.js
@@ -0,0 +1,9 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyDsUgrgjuJ4Fs76gkPDqLWBsX7HyFTIdWI",
+  authDomain: "my-github-page-db-d0536.firebaseapp.com",
+  databaseURL: "https://my-github-page-db-d0536-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "my-github-page-db-d0536",
+  storageBucket: "my-github-page-db-d0536.firebasestorage.app",
+  messagingSenderId: "46067744539",
+  appId: "1:46067744539:web:bfdace94a8d85a7e3fddbe"
+};
