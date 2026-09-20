5ae56eb6d95c74185cd838c22931b1e6a6dac1ae
TimurVetrov <140169369+VetrovTimur@users.noreply.github.com>
2026-09-20 12:12:24 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,5 @@
 import { firebaseConfig } from './firebase-config.js';
-import { initializeApp } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-app.js";
-import { getDatabase, ref, onValue } from "https://www.gstatic.com/firebasejs/12.18.0/firebase-database.js";
+import { initializeApp } from "https://www.gstatic.com/firebasejs/12.19.0/firebase-app.js";
 
-// Инициализация Firebase
 const app = initializeApp(firebaseConfig);
-const db = getDatabase(app);
-
-// Ссылка на корень базы данных
-const dbRef = ref(db, '/');
-
-// Подписываемся на изменения
-onValue(dbRef, (snapshot) => {
-    const data = snapshot.val();
-    document.getElementById('status').textContent = JSON.stringify(data);
-}, (error) => {
-    console.error("Ошибка:", error);
-    document.getElementById('status').textContent = "Ошибка подключения";
-});
+console.log("Firebase успешно подключен!");
